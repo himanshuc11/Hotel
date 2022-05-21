cc522c6553f6ee80a4eec31891cbde7ce585c1f9
himanshuc11 <himanshuchhatpar@gmail.com>
2022-05-21 11:32:42 +0530

    Initial setup with chakra, firebase, react-router

---
+++ src/Settings/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCC341eX2sFrW1CKXEy4HHyZQVM6T6qqGk',
+  authDomain: 'hotel-50de5.firebaseapp.com',
+  projectId: 'hotel-50de5',
+  storageBucket: 'hotel-50de5.appspot.com',
+  messagingSenderId: '541414603937',
+  appId: '1:541414603937:web:555edec3823a7be3d3c4e0',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
